1273fb999f6c0cd3c4e176ff2646d33a0ec3458a
Nikita12G <gulyi08@mail.ru>
2022-02-04 16:35:19 +0700

    Initial Commit

---
+++ CarApp(RxSwift)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarApp(RxSwift)
+//
+//  Created by Никита Гуляев on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
